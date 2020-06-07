dce259b2b24449ed9ab1cb0f07b050b6a694f765
Irshad <irshad.mohd@r-indventures.com>
2020-06-07 12:34:42 +0530

    added profile api

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,41 +1,20 @@
 'use strict'
 
-const account_helper = require('../helper/user/account')
 const Joi = require('@hapi/joi')
-const user_auth_controller = require('../controllers/user/user_auth_controller')
+const user_controller = require('../controllers/user/user_controller')
 const fail_action = require('../helper/error/fail_action')
 
 let mx = [
   {
-    method: 'POST',
-    path: '/api/user/auth/signup',
+    method: 'GET',
+    path: '/api/user/profile',
     config: {
-      pre: [{ method: account_helper.verify_unique_user }],
-      handler: user_auth_controller.signup,
+      handler: user_controller.profile,
       validate: {
-        payload: Joi.object({
-          first_name: Joi.string().min(2).max(60).required(),
-          last_name: Joi.string().allow('').max(60),
-          email: Joi.string().email().required(),
-          password: Joi.string().required(),
-          above18_fair_use: Joi.boolean().required(),
-          mobile: Joi.string()
-        }),
-        failAction: fail_action
-      }
-    }
-  },
-  {
-    method: 'POST',
-    path: '/api/user/auth/login',
-    config: {
-      handler: user_auth_controller.login,
-      validate: {
-        payload: Joi.object({
-          email: Joi.string().email().required(),
-          password: Joi.string().required()
-        }),
-        failAction: fail_action
+        failAction: fail_action,
+        headers: Joi.object({
+          authorization: Joi.string().required()
+        }).unknown()
       }
     }
   }
