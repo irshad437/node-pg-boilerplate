72b477a871e878aef50496ce6f60457341708895
Irshad <irshad.mohd@r-indventures.com>
2020-06-07 12:34:29 +0530

    changed filename

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,22 +1,44 @@
+'use strict'
+
+const account_helper = require('../helper/user/account')
 const Joi = require('@hapi/joi')
+const user_auth_controller = require('../controllers/user/user_auth_controller')
 const fail_action = require('../helper/error/fail_action')
 
-module.exports = [
+let mx = [
   {
-    method: 'GET',
-    path: '/v1.0/user/account/create',
-    options: {
-      handler: function (req, h) {
-        return 'hello there'
-      },
-      tags: ['api', 'user', 'account'],
-      description: 'Create user account',
+    method: 'POST',
+    path: '/api/user/auth/signup',
+    config: {
+      pre: [{ method: account_helper.verify_unique_user }],
+      handler: user_auth_controller.signup,
       validate: {
-        query: Joi.object({
-          limit: Joi.number()
-        }),
-        failAction: fail_action
+        failAction: fail_action,
+        payload: Joi.object({
+          first_name: Joi.string().min(2).max(60).required(),
+          last_name: Joi.string().allow('').max(60),
+          email: Joi.string().email().required(),
+          password: Joi.string().required(),
+          above18_fair_use: Joi.boolean().required(),
+          mobile: Joi.string()
+        })
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/api/user/auth/login',
+    config: {
+      handler: user_auth_controller.login,
+      validate: {
+        failAction: fail_action,
+        payload: Joi.object({
+          email: Joi.string().email().required(),
+          password: Joi.string().required()
+        })
       }
     }
   }
 ]
+
+module.exports = mx
